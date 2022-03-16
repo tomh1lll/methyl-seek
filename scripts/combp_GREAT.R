0c2af1c1d22c722275fb50bf065ee2cb40868004
Tom Hill <tom.hill@nih.gov>
2022-03-16 15:49:49 -0400

    Update combp_GREAT.R

--- scripts/combp_GREAT.R
+++ scripts/combp_GREAT.R
@@ -8,12 +8,12 @@ input = read.delim(args[1],h=F)
 
 input2 = input[,c(1,2,3)]
 
-job = submitGreatJob(input2)
+job = submitGreatJob(input2,species = "hg38")
 
 tb = getEnrichmentTables(job)
 
-write.table(tv[[1]],file = args[3])
-write.table(tv[[2]],file = args[4])
+write.table(tb[[1]],file = args[3])
+write.table(tb[[2]],file = args[4])
 
 jpeg(file=args[2],width = 12,height = 6,units = "in",res = 300)
 res = plotRegionGeneAssociationGraphs(job)
