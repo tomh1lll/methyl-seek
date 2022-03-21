06dd634eb5aed4c2bc7d297460901a9fab82d656
Tom Hill <tom.hill@nih.gov>
2022-03-21 16:22:26 -0400

    Update combp_GREAT.R

--- scripts/combp_GREAT.R
+++ scripts/combp_GREAT.R
@@ -10,7 +10,7 @@ input2 = input[,c(1,2,3)]
 
 job = submitGreatJob(input2,species = "hg38")
 
-tb = getEnrichmentTables(job)
+tb = getEnrichmentTables(job,download_by = "tsv")
 
 write.table(tb[[1]],file = args[3])
 write.table(tb[[2]],file = args[4])
