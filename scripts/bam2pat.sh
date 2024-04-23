69935a9a595185412c45020a99813fd6ba37dbbb
Tom Hill <tom.hill@nih.gov>
2024-04-23 11:56:10 -0400

    Update bam2pat.sh

--- scripts/bam2pat.sh
+++ scripts/bam2pat.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 export PATH=${PATH}:/data/NHLBI_IDSS/references/UXM
@@ -6,14 +7,17 @@ module load samtools bedtools bamtools
 
 in=$1
 
-out=$2
+#out=$2
 
-outdir=$3
+outdir=$2
 
 mkdir -p ${outdir}
 
-samtools sort -@ 12 --reference /data/NHLBI_IDSS/references/Bismark_Genomes/hg38/genome.fa ${in} > ${out}
+#samtools sort -@ 12 --reference /data/NHLBI_IDSS/references/Bismark_Genomes/hg38/genome.fa ${in} > ${out}
 
-samtools index ${out}
+#samtools index ${out}
 
-wgbstools bam2pat --genome hg38 --out_dir ${outdir} -@ 12 ${out}
+wgbstools bam2pat --genome hg38 --out_dir ${outdir} -@ 12 ${in}
+
+#rm ${out}*
+
